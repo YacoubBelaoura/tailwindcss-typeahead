db627d3448b561f7c89e5af77de5dbe139eb34b9
Yacoub Belaoura <yacoubbelaoura@gmail.com>
2019-06-17 19:09:47 +0100

    add tailwindcss typeahead

--- src/bulma/Typeahead.vue
+++ src/bulma/Typeahead.vue
@@ -11,20 +11,30 @@
                 :disabled="!query"
                 manual>
                 <template v-slot:trigger="{ open }">
-                    <div class="control has-icons-left has-icons-right"
+                    <!-- <div class="control has-icons-left has-icons-right" -->
+                    <div class=" relative ltr:text-left rtl:text-right flex content-center"
                         :class="{ 'is-loading': loading }">
-                        <input class="input is-fullwidth"
-                            :class="[{ 'is-rounded': isRounded }, { 'is-danger': hasError }]"
+                        <!-- <input class="input is-fullwidth" -->
+                        <!-- TODO: add Diffrent colored shadow ? -->
+                        <input class=" px-10  w-full block bg-white h-10
+                        border border-gray-860 shadow-inner focus:shadow-outline hover:border-gray-500 hover:shadow text-base"
+                            :class="[{ 'is-rounded rounded-full': isRounded }, { 'is-danger border-red-500': hasError }]"
                             type="text"
                             :disabled="disabled"
                             :placeholder="i18n(placeholder)"
                             @keyup="open"
                             v-bind="inputBindings"
                             v-on="inputEvents">
-                        <span class="icon is-small is-left">
+                        <!-- <span class="icon is-small is-left"> -->
+                        <span class="items-center inline-flex justify-center
+                        absolute top-0 ltr:left-0 rtl:right-0
+                        text-gray-500 w-10 h-10">
                             <fa icon="search"/>
                         </span>
-                        <span class="icon is-small is-right clear-button"
+                        <!-- <span class="icon is-small is-right clear-button" -->
+                        <span class="items-center inline-flex justify-center
+                        absolute top-0 rtl:left-0 ltr:right-0
+                        focus:text-gray-480 text-gray-500 w-10 h-10"
                             v-on="clearBindings"
                             v-if="query && !loading">
                             <a class="delete is-small"/>
@@ -36,10 +46,13 @@
                         :items="items"/>
                 </template>
                 <template v-slot:options>
-                    <a class="dropdown-item option"
+                    <!-- <a class="dropdown-item option" -->
+                    <a class="dropdown-item option ltr:text-left rtl:text-right
+                                rounded-sm block py-2 px-3 whitespace-no-wrap w-full
+                                cursor-pointer no-underline relative text-sm hover:bg-white-smoke"
                         v-for="(item, index) in items"
                         :key="index"
-                        :class="{ 'is-active': index === currentIndex }"
+                        :class="{ 'is-active bg-blue-500 hover:bg-blue-400 font-semibold text-white': index === currentIndex }"
                         v-on="itemEvents(index)">
                         <slot name="option"
                             :highlight="highlight"
@@ -49,13 +62,17 @@
                         </slot>
                     </a>
                     <template v-if="!items.length">
-                        <a class="dropdown-item"
+                        <!-- <a class="dropdown-item" -->
+                        <a class="dropdown-item  ltr:text-left rtl:text-right rounded-sm block py-2 px-3
+                                whitespace-no-wrap w-full cursor-pointer no-underline relative text-sm hover:bg-white-smoke"
                             v-if="loading">
                             <span v-if="loading">
                                 {{ i18n(searching) }}
                             </span>
                         </a>
-                        <a class="dropdown-item"
+                        <!-- <a class="dropdown-item" -->
+                        <a class="dropdown-item  rounded-sm block py-2 px-3 whitespace-no-wrap w-full ltr:text-left rtl:text-right
+                                cursor-pointer no-underline relative text-sm hover:bg-white-smoke"
                             v-else-if="query">
                             <span>
                                 {{ i18n(noResults) }}
@@ -129,9 +146,10 @@ export default {
             width: 100%;
 
             .dropdown-content {
+            // .overflow-y-auto {
                 width: 100%;
-                max-height: 20em;
-                overflow-y: scroll;
+                max-height: 20rem;
+                // overflow-y: scroll;
                 a.dropdown-item {
                     width: 100%;
                     text-overflow: ellipsis;
@@ -143,5 +161,8 @@ export default {
     }
     .control.has-icons-right .icon.clear-button {
         pointer-events: all;
+    }
+    .max-h-20 {
+        max-height: 20rem;
     }
 </style>
